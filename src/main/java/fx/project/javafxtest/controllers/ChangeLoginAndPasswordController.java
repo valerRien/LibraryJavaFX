e17b1e96ea41607e3526ba33b157322f974ff741
valerRien <valeracedin@yandex.ru>
2023-05-30 20:00:16 +0300

    added "change log and pass" for admin class

--- src/main/java/fx/project/javafxtest/controllers/ChangeLoginAndPasswordController.java
+++ src/main/java/fx/project/javafxtest/controllers/ChangeLoginAndPasswordController.java
@@ -1,17 +1,25 @@
 package fx.project.javafxtest.controllers;
 
+import fx.project.javafxtest.animations.Shake;
+import fx.project.javafxtest.dao.AdminDAO;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class ChangeLoginAndPasswordController implements Initializable {
 
+    public static final String PATH = "/fx/project/javafxtest/ChangeLoginAndPassword.fxml";
+    private ClickController clickController = new ClickController();
+
+    private AdminDAO adminDAO = new AdminDAO();
     @FXML
     private ResourceBundle resources;
 
@@ -34,16 +42,63 @@ public class ChangeLoginAndPasswordController implements Initializable {
     private PasswordField oldPasswordField;
 
     @FXML
-    void initialize() {
+    private Button saveEditing;
 
-    }
-
-    public void mouseReleased(MouseEvent mouseEvent) {
-
+    public ChangeLoginAndPasswordController() throws Exception {
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        saveEditing.setOnAction(actionEvent -> {
+            boolean shouldBeEdit = false;
+            int adminId;
+            String newLogin;
+            String newPassword;
+            try {
+                if ((adminId = adminDAO.verify(newLogin = getOldLogin(), newPassword = getOldPassword())) != -1) {
+                    shouldBeEdit = true;
+                } else {
+                    new Shake(saveEditing).playAnim();
+                }
 
+                if (shouldBeEdit) {
+                    if (!getNewLogin().isEmpty() && !getNewPassword().isEmpty()) {
+                        newLogin = getNewLogin();
+                        newPassword = getNewPassword();
+                    } else if (!getNewLogin().isEmpty()) {
+                        newLogin = getNewLogin();
+                    } else if (!getNewPassword().isEmpty()){
+                        newPassword = getNewPassword();
+                    }
+
+                    adminDAO.updateAdmin(adminId, newLogin, newPassword);
+                    clickController.mouserReleasedOnButton(saveEditing, MainAppController.PATH);
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+    }
+
+    public void mouseReleasedHomeButton(MouseEvent mouseEvent) {
+        clickController.mouseReleasedOnImage(mouseEvent, homeButton, MainAppController.PATH);
+    }
+
+    private String getOldLogin() {
+        return oldLoginField.getText();
+    }
+
+    private String getNewLogin() {
+        return newLoginField.getText();
+    }
+
+    private String getOldPassword() {
+        return oldPasswordField.getText();
+    }
+
+    private String getNewPassword() {
+        return newPasswordField.getText();
     }
 }
