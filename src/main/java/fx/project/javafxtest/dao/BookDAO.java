3ed354662cc1c1d5bfa4ff5568dc56295d2dad8e
valerRien <valeracedin@yandex.ru>
2023-05-30 15:25:55 +0300

    added Book model and BookDAO

--- src/main/java/fx/project/javafxtest/dao/BookDAO.java
+++ src/main/java/fx/project/javafxtest/dao/BookDAO.java
@@ -1,11 +1,13 @@
 package fx.project.javafxtest.dao;
 
 import fx.project.javafxtest.config.ConfigDB;
+import fx.project.javafxtest.models.Book;
+import fx.project.javafxtest.models.BookProxy;
 import fx.project.javafxtest.models.Reader;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class BookDAO {
@@ -14,10 +16,90 @@ public class BookDAO {
     public BookDAO() throws Exception {
     }
 
-    public List<Reader> getTimeOutReaders() {
-        return new ArrayList<>(List.of(
-                new Reader("Testname", "TestSurname", "TestEmail", "TestPhone", "TestGender"),
-                new Reader("Testname", "TestSurname", "TestEmail", "TestPhone", "TestGender"),
-                new Reader("Testname", "TestSurname", "TestEmail", "TestPhone", "TestGender")));
+    public void addBook(Book book) throws SQLException {
+        String query = "INSERT INTO BOOKS (TITLE, AUTHOR, YEAR_OF_PRODUCTION) VALUES (?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, book.getTitle());
+        statement.setString(2, book.getAuthor());
+        statement.setInt(3, book.getYearOfProduction());
+
+        statement.executeUpdate();
+    }
+
+    public List<Reader> getTimeOutReaders() throws SQLException {
+        String query = "select * from BOOKS JOIN READERS ON BOOKS.READER_ID = READERS.ID where DATEDIFF('DAY',BOOKS.SUBMISSION_DATE, ?) > 30";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setDate(1, new Date(System.currentTimeMillis()));
+        ResultSet resultSet = statement.executeQuery();
+        List<Reader> readers = new ArrayList<>();
+        while (resultSet.next()) {
+            Book book = new Book();
+            book.setId(resultSet.getInt("books.id"));
+            book.setReader_id(resultSet.getInt("reader_id"));
+            book.setTitle(resultSet.getString("title"));
+            book.setAuthor(resultSet.getString("author"));
+            book.setId(resultSet.getInt("year_of_production"));
+            book.setPreviousReader(resultSet.getInt("previous_reader"));
+            book.setSubmission_date(resultSet.getDate("submission_date"));
+
+            Reader reader = new Reader();
+            reader.setId(resultSet.getInt("readers.id"));
+            reader.setName(resultSet.getString("name"));
+            reader.setSurname(resultSet.getString("surname"));
+            reader.setEmail(resultSet.getString("email"));
+            reader.setPhoneNumber(resultSet.getString("phone_number"));
+            reader.setGender(resultSet.getString("gender"));
+
+            reader.setBook(book);
+            readers.add(reader);
+        }
+
+        return readers;
+    }
+
+    public void assignBook(Reader reader, Book book) throws SQLException {
+        String query = "UPDATE TABLE BOOKS SET PERSON_ID=? WHERE ID=?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setInt(1, reader.getId());
+        statement.setInt(2, book.getId());
+
+        statement.executeUpdate();
+    }
+
+    public List<Book> getAllBooksWithReaders() throws SQLException {
+        String query = "SELECT * FROM BOOKS LEFT JOIN READERS ON BOOKS.READER_ID=READERS.ID";
+        PreparedStatement statement = connection.prepareStatement(query);
+
+        ResultSet resultSet = statement.executeQuery();
+        List<Book> bookList = new ArrayList<>();
+        while (resultSet.next()) {
+            Book book = new Book();
+            book.setTitle(resultSet.getString("title"));
+            book.setAuthor(resultSet.getString("author"));
+            book.setSubmission_date(resultSet.getDate("submission_date"));
+
+            Reader reader = new Reader();
+            reader.setName(resultSet.getString("name"));
+            reader.setSurname(resultSet.getString("surname"));
+
+            book.setReader(reader);
+            bookList.add(book);
+        }
+
+        return bookList;
+    }
+
+    public List<BookProxy> getBooksProxy() throws SQLException {
+        List<Book> booksList = getAllBooksWithReaders();
+        List<BookProxy> proxyBooksList = new ArrayList<>();
+        for (Book book : booksList) {
+            proxyBooksList.add(new BookProxy(book));
+        }
+        return proxyBooksList;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(new BookDAO().getAllBooksWithReaders());
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(new java.util.Date()));
     }
 }
--- src/main/java/fx/project/javafxtest/models/Book.java
+++ src/main/java/fx/project/javafxtest/models/Book.java
@@ -1,4 +1,96 @@
 package fx.project.javafxtest.models;
 
+import java.util.Date;
+
 public class Book {
+
+    private int id;
+    private int reader_id;
+    private String title;
+    private String author;
+
+    private int yearOfProduction;
+
+    private int previousReader;
+    private Date submission_date;
+
+    private Reader reader;
+
+    public Reader getReader() {
+        return reader;
+    }
+
+    public void setReader(Reader reader) {
+        this.reader = reader;
+    }
+
+    public Book() {
+    }
+
+    public Book(String title, String author, int yearOfProduction) {
+        this.title = title;
+        this.author = author;
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getReader_id() {
+        return reader_id;
+    }
+
+    public void setReader_id(int reader_id) {
+        this.reader_id = reader_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public int getPreviousReader() {
+        return previousReader;
+    }
+
+    public void setPreviousReader(int previousReader) {
+        this.previousReader = previousReader;
+    }
+
+    public Date getSubmission_date() {
+        return submission_date;
+    }
+
+    public void setSubmission_date(Date submission_date) {
+        this.submission_date = submission_date;
+    }
+
+    @Override
+    public String toString() {
+        return title + ", " + author + ", " + yearOfProduction;
+    }
 }
